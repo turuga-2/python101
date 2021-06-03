7f38d01e7a870d569f7fcbb8e6f42adc0e5883ad
Paul K. Korir <31738053+paulkorir@users.noreply.github.com>
2021-06-03 13:13:05 +0100

    Notes

--- problem1.py
+++ problem1.py
@@ -6,6 +6,7 @@ def main():
     name = input("Please enter your full name")
     age = int(input("Please enter your current age"))
     birth_year = 2021 - age
+    # perhaps you can come up with a better name for this variable so that the reader will know what it is immediately 
     seventy_seven = birth_year + 77
 
     print(f'Hello my name is {name} and I am {age} years old.')
